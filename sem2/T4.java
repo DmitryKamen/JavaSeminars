da15ca0386f68177183625d777e2bb428ccfea13
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-12-22 21:52:16 +0200

    sem2

---
+++ sem2/T4.java
@@ -0,0 +1,29 @@
+package JavaSeminars.sem2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class T4 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("example.txt");
+
+            int i;
+            while ((i = fis.read()) != -1) {
+                // Читаем и выводим содержимое файла по одному байту
+                System.out.print((char) i);
+            }
+            fis.close();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object.dat"));
+//        oos.writeObject(myObject);
+//        oos.close(); 
+    }
+    
+}
